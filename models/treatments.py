323e0c392e91e9789b3c13c6c1881d3e1edb2503
Jeremias <jeremias.palacios.com.ar>
2023-05-11 08:57:56 -0300

    no editable data

--- models/treatments.py
+++ models/treatments.py
@@ -7,6 +7,6 @@ class HospitalTreatments(models.Model):
     _description = 'Treatments Record'
     _rec_name = 'treatments_name'
 
-    treatments_code = fields.Char(string="Treatment code", readonly=True)
-    treatments_name = fields.Char(string="Treatment name", readonly=True)
-    doctor = fields.Char(string="Treating physician", readonly=True)
+    treatments_code = fields.Char(string="Treatment code")
+    treatments_name = fields.Char(string="Treatment name")
+    doctor = fields.Char(string="Treating physician")
